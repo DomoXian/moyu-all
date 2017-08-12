c530d19119b4b95fc25727c1825c0cf9461203a1
李贤 <xianguo@maihaoche.com>
2017-08-13 01:06:28 +0800

    随便

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ PROJECT_HOME=/home/admin/app
 TARGET_HOME=$PROJECT_HOME/moyu-all/moyu-core/target
 TARGET_NAME=moyu.jar
 cd /home/admin/app/moyu-all
-mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
+#mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
 #cd $TARGET_HOME
 #MOYU_PID=`ps -ef | grep "moyu" | grep -v grep | awk '{print $2}'`
 #echo "Moyu PID = $MOYU_PID"
