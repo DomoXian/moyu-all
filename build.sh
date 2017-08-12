81dc604e9b4b669e512c872e474101cfa70e4f0f
李贤 <xianguo@maihaoche.com>
2017-08-13 01:18:02 +0800

    ddd

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ TARGET_HOME=$PROJECT_HOME/moyu-all/moyu-core/target
 TARGET_NAME=moyu.jar
 cd /home/admin/app/
 pwd
+ls
 #cd /home/admin/app/moyu-all
 #mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
 #cd $TARGET_HOME
