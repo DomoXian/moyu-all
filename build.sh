eeed862678a2129852f4e5196bdf1daed5b6cb96
李贤 <xianguo@maihaoche.com>
2017-08-13 00:36:28 +0800

    cao

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ TARGET_HOME=$PROJECT_HOME/moyu-all/moyu-core/target
 TARGET_NAME=moyu.jar
 cd /home/admin/app
 mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
-cd $TARGET_HOME
-MOYU_PID=`ps -ef | grep "moyu" | grep -v grep | awk '{print $2}'`
-echo "Moyu PID = $MOYU_PID"
-kill -9 MOYU_PID
-java -jar $TARGET_HOME/$TARGET_NAME --spring.profiles.active=dev > ./moyu.log &
+#cd $TARGET_HOME
+#MOYU_PID=`ps -ef | grep "moyu" | grep -v grep | awk '{print $2}'`
+#echo "Moyu PID = $MOYU_PID"
+#kill -9 MOYU_PID
+#java -jar $TARGET_HOME/$TARGET_NAME --spring.profiles.active=dev > ./moyu.log &
