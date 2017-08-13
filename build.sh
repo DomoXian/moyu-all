47f06d2eda8c07d6b7aed00a41a3845ea7e5755e
李贤 <xianguo@maihaoche.com>
2017-08-13 09:39:05 +0800

    ffff

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 PROJECT_NAME=moyu
-#PROJECT_HOME=/home/admin/app/
+PROJECT_HOME=/home/admin/app/
 JAR_DIR=$PROJECT_NAME-core/target
 JAR_NAME=$PROJECT_NAME.jar
-#cd $PROJECT_HOME
+cd $PROJECT_HOME
 ls
 mvn clean install -Dmaven.test.skip&& mvn package -Dmaven.test.skip
 cd $JAR_DIR
